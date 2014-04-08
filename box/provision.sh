e2fdb0cc5aa55b6be0a4fd27579dc72a4a4ad476
Mads Flensted-Urech <mfu@coadu.com>
2014-04-08 23:25:32 +0200

    Use git clones for libsodium, libzmq and czmq

--- box/provision.sh
+++ box/provision.sh
@@ -1,37 +1,33 @@
 #!/bin/bash
 
-sudo apt-get install -y git curl wget net-tools build-essential pkg-config libtool autoconf automake
+sudo apt-get install -y git curl wget net-tools
+sudo apt-get install -y build-essential pkg-config libtool autoconf automake
 sudo apt-get install -y vim emacs
 
 sudo apt-get install -y uuid-dev
 
-work=/tmp/zmq
-mkdir -p $work
+work=/usr/src
 
 cd $work
-wget https://download.libsodium.org/libsodium/releases/libsodium-0.4.5.tar.gz
-wget https://download.libsodium.org/libsodium/releases/libsodium-0.4.5.tar.gz.sig
-# TODO: Can't get this to work, dig +dnssec +short txt <file>.download.libsodium.org, see https://github.com/jedisct1/libsodium#installation
-tar xzvf libsodium-0.4.5.tar.gz
-cd libsodium-0.4.5
+git clone http://github.com/jedisct1/libsodium.git
+cd libsodium
 ./autogen.sh
-./configure && make && make check && sudo make install
+./configure && make check
+make install
 ldconfig
 
 cd $work
-wget http://download.zeromq.org/zeromq-4.0.4.tar.gz
-tar xzvf zeromq-4.0.4.tar.gz
-cd zeromq-4.0.4
+git clone http://github.com/zeromq/libzmq.git
+cd libzmq
 ./autogen.sh
-./configure && make && sudo make install
+./configure && make check
+make install
 ldconfig
 
 cd $work
-wget https://github.com/zeromq/czmq/archive/v2.1.0.tar.gz
-tar xzvf v2.1.0.tar.gz
-cd czmq-2.1.0
+git clone http://github.com/zeromq/czmq.git
+cd czmq
 ./autogen.sh
-./configure && make && sudo make install
+./configure && make check
+make install
 ldconfig
-
-rm -rf $work
